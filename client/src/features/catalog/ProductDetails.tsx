be95393e0a6e230ac84aafe6491df4e0febb5119
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-05-22 21:14:11 +0200

    get data product for details

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import type { Product } from "../../app/models/product";
+
 export default function ProductDetails() {
-  return <div>Product Details</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  useEffect(() => {
+    fetch(`https://localhost:5001/api/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.log(error));
+  }, [id]);
+  return <div>{product?.name}</div>;
 }
