a7462d4e51dfa3d306c9b1123160dd52376a7d32
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2025-06-08 16:16:47 +0200

    error and debug is done.

---
+++ client/src/app/error/NotFound.tsx
@@ -0,0 +1,27 @@
+import { SearchOff } from "@mui/icons-material";
+import { Button, Paper, Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function NotFound() {
+  return (
+    <Paper
+      sx={{
+        height: 400,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        p: 6,
+      }}
+    >
+      <SearchOff sx={{ fontSize: 100 }} color="primary" />
+      <Typography gutterBottom variant="h3">
+        Oops we could not find what you are looking for.
+      </Typography>
+      <Button fullWidth LinkComponent={Link} to="/catalog">
+        Go Back To Shop
+      </Button>
+    </Paper>
+  );
+}
--- client/src/app/store/store.ts
+++ client/src/app/store/store.ts
@@ -5,6 +5,7 @@ import counterReducer, {
 import { useDispatch, useSelector } from "react-redux";
 import { catalogApi } from "../../features/catalog/catalogApi";
 import { uiSlice } from "../layout/uiSlice";
+import { errorApi } from "../../features/about/errorApi";
 
 export function configureTheStore() {
   return legacy_createStore(counterReducer);
@@ -12,11 +13,12 @@ export function configureTheStore() {
 export const store = configureStore({
   reducer: {
     [catalogApi.reducerPath]: catalogApi.reducer,
+    [errorApi.reducerPath]: errorApi.reducer,
     counter: counterSlice.reducer,
     ui: uiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(catalogApi.middleware),
+    getDefaultMiddleware().concat(catalogApi.middleware, errorApi.middleware),
 });
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
